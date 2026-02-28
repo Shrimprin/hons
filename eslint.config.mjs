8584bd459bb85c6cb2516a3c341a4a5b5d5cdb94
Kuroneko <ken.aster8@gmail.com>
2026-02-28 23:25:25 +0900

    eslint, prettier設定を修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,29 @@
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
-import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigNextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import eslintConfigNextTypeScript from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import { defineConfig } from 'eslint/config';
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-  eslintPluginBetterTailwindcss.configs["recommended-warn"],
+  ...eslintConfigNextCoreWebVitals,
+  ...eslintConfigNextTypeScript,
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    files: ['**/*.{jsx,tsx}'],
+    plugins: {
+      'better-tailwindcss': eslintPluginBetterTailwindcss,
+    },
+    rules: {
+      ...eslintPluginBetterTailwindcss.configs['correctness-warn'].rules,
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/globals.css',
+      },
+    },
+  },
   eslintConfigPrettier,
 ]);
 
